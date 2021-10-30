e26f982d0c7998cdf0e2711a7c7f85694a1cb825
Sibz <12050358+Sibz@users.noreply.github.com>
2021-10-31 08:27:07 +1100

    refactor: add use statements to improve readability

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+use hex::encode;
+use ring::hmac::{sign, Key, HMAC_SHA256};
+
 pub fn get_sha256_hmac_as_hex_str(msg: &str, secret: &str) -> String
 {
-    let key = ring::hmac::Key::new(ring::hmac::HMAC_SHA256, secret.as_bytes());
-    let sign = ring::hmac::sign(&key, msg.as_bytes());
-    hex::encode(sign.as_ref())
+    let key = Key::new(HMAC_SHA256, secret.as_bytes());
+    let sign = sign(&key, msg.as_bytes());
+    encode(sign.as_ref())
 }

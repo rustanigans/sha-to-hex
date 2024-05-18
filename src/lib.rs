fde3660eacb7e3c79e30b7c0aeff73dd2f7d7048
Steven <12050358+Sibz@users.noreply.github.com>
2024-05-18 03:47:59 +0000

    remove duplicate test

--- src/lib.rs
+++ src/lib.rs
@@ -44,11 +44,3 @@ pub fn get_sha256_hmac_as_hex_str(msg: &str, secret: &str) -> String
 {
     encode(msg, secret)
 }
-
-#[test]
-fn test () {
-    let msg = "The quick brown fox jumps over the lazy dog";
-    let secret = "543jklefdsjio342jiofdsoij5234orfjdso";
-    assert_eq!(encode(msg, secret),
-               "277288e6f134b423756870fbf222f56c8e17d59d9a2df0f8c535a5ab3f84a9bb");
-}

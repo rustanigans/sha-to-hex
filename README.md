06bf953412bcabc888996eac96b98995ea260bc4
Steven <12050358+Sibz@users.noreply.github.com>
2024-05-18 04:03:59 +0000

    feat: rename function, support bytes and inline always (#1)
    * update readme
    
    * renamed function to encode, creating alias with old name
    
    * add encode_bytes to support using raw bytes as message/secret
    
    * add tests in integration tests folder
    
    * remove unit test
    
    * update version

--- README.md
+++ README.md
@@ -2,4 +2,17 @@
 
 Simple function to encode string using secret and return hex string
 
-For usage see doc comment in [lib.rs](src/lib.rs)
+```
+use sha_to_hex::encode;
+
+let encoded = encode(msg, secret);
+```
+
+To encode bytes use encode_bytes:
+```
+use sha_to_hex::encode_bytes;
+
+let encoded = encode_bytes(msg_as_bytes, secret_as_bytes);
+```
+
+For full usage see docs.rs: [encode](https://docs.rs/sha-to-hex/latest/sha_to_hex/fn.encode.html)
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
-use hex::encode;
+use const_hex::encode as hex_encode;
 use ring::hmac::{sign, Key, HMAC_SHA256};
-/// Encode msg using secret return hex string
+/// Encode a message using secret with HMAC-SHA256 encryption
+/// 
+/// returns a hex string
 ///
-/// # Usage
-/// `get_sha256_hmac_as_hex_str(msg: &str, secret: &str) -> String`
+/// ### Usage
+/// `encode(msg: &str, secret: &str) -> String`
 ///
-/// # Example
+/// ### Example
 /// ```
-/// use sha_to_hex::get_sha256_hmac_as_hex_str as encode;
+/// use sha_to_hex::encode;
 ///
 /// let msg = "The quick brown fox jumps over the lazy dog";
 /// let secret = "543jklefdsjio342jiofdsoij5234orfjdso";
@@ -15,9 +17,30 @@ use ring::hmac::{sign, Key, HMAC_SHA256};
 /// assert_eq!(encode(msg, secret),
 ///            "277288e6f134b423756870fbf222f56c8e17d59d9a2df0f8c535a5ab3f84a9bb")
 /// ```
+#[inline(always)]
+pub fn encode(msg: &str, secret: &str) -> String
+{
+    encode_bytes(msg.as_bytes(), secret.as_bytes())
+}
+
+/// Encode a message using secret with HMAC-SHA256 encryption
+/// 
+/// returns a hex string
+/// 
+/// ### Usage
+/// `encode_bytes(msg: &[u8], secret: &[u8]) -> String`
+#[inline(always)]
+pub fn encode_bytes(msg: &[u8], secret: &[u8]) -> String
+{
+    hex_encode(&sign(&Key::new(HMAC_SHA256, secret), msg))
+}
+
+/// [Deprecated] Encode msg using secret return hex string
+/// This is just alias of `encode` for backward compatibility
+/// 
+/// use `encode` instead
+#[inline(always)]
 pub fn get_sha256_hmac_as_hex_str(msg: &str, secret: &str) -> String
 {
-    let key = Key::new(HMAC_SHA256, secret.as_bytes());
-    let sign = sign(&key, msg.as_bytes());
-    encode(sign.as_ref())
+    encode(msg, secret)
 }

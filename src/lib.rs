352bf89280b0e885a66c4ac90a4ed59076dd4b09
Steven <12050358+Sibz@users.noreply.github.com>
2024-05-18 03:11:04 +0000

    chore: added test

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,10 @@ use const_hex::encode;
 use ring::hmac::{sign, Key, HMAC_SHA256};
 /// Encode msg using secret return hex string
 ///
-/// # Usage
+/// ### Usage
 /// `get_sha256_hmac_as_hex_str(msg: &str, secret: &str) -> String`
 ///
-/// # Example
+/// ### Example
 /// ```
 /// use sha_to_hex::get_sha256_hmac_as_hex_str as encode;
 ///
@@ -20,4 +20,12 @@ pub fn get_sha256_hmac_as_hex_str(msg: &str, secret: &str) -> String
     let key = Key::new(HMAC_SHA256, secret.as_bytes());
     let sign = sign(&key, msg.as_bytes());
     encode(sign.as_ref())
+}
+
+#[test]
+fn test () {
+    let msg = "The quick brown fox jumps over the lazy dog";
+    let secret = "543jklefdsjio342jiofdsoij5234orfjdso";
+    assert_eq!(get_sha256_hmac_as_hex_str(msg, secret),
+               "277288e6f134b423756870fbf222f56c8e17d59d9a2df0f8c535a5ab3f84a9bb");
 }
